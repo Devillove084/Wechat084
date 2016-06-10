2cc3a988d3a694226d30c14eb5406415a183b5d6
Devillove084 <Devillove084@users.noreply.github.com>
2016-06-10 20:48:52 +0800

    for test
    test

---
+++ OAuth2InterceptorServlet.java
@@ -0,0 +1,18 @@
+package servlet;
+
+import java.io.IOException;
+
+
+/**
+ * Created by root on 6/10/16.
+ */
+public class OAuth2InterceptorServlet extends javax.servlet.http.HttpServlet {
+    public void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    public void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+
+    }
+}
